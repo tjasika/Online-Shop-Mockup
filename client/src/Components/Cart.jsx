be00351e817c02cd73245c54f46c87f603a9f387
Tjaša <tjasazzeleznik@gmail.com>
2025-09-30 16:56:15 +0200

    Cart styling

--- client/src/Components/Cart.jsx
+++ client/src/Components/Cart.jsx
@@ -20,16 +20,29 @@ export default function Cart({cartItems, removeFromCart, updateQuantity}) {
                         <span>Shopping cart is empty.</span>
                     ) : (
                         <>
+                        <div className="flex flex-col gap-3">
                         {cartItems.map((item) => (
-                            <div key={item.cart_item_id}>
-                                <img src={item.product_image} alt={item.product_name} className="w-20 h-20 object-cover" />
-                                <h3>{item.product_name}</h3>
-                                <p>Price: ${item.product_price}</p>
-                                <p>Size: {item.size_name}</p>
-                                <p>Color: {item.color_name}</p>
-                                <p>Quantity: {item.quantity}</p>
+                            <div key={item.cart_item_id} className="w-200 flex flex-row gap-5 h-50">
+                                <div>
+                                    <img src={item.product_image} alt={item.product_name} className="w-30 h-50 object-cover" />
+                                </div>
+                                <div className="flex flex-col justify-center w-50">
+                                    <p className="uppercase font-instrument text-md">{item.product_name}</p>
+                                    <p className="font-medium text-lg">${item.product_price}</p>
+                                    <p>Size: {item.size_name}</p>
+                                    <p>Color: {item.color_name}</p>
+                                </div>
+                                <div className="flex flex-col justify-center ml-3">
+                                    <span className="pb-2">Quantity:</span>
+                                    <div className="border-1 rounded-3xl flex flex-row gap-4 w-30 p-2 justify-center">
+                                        <button onClick={()=>updateQuantity(item.cart_item_id, item.quantity + 1)} className="hover:cursor-pointer"><img src="/icons/plus.svg"></img></button>
+                                        <span>{item.quantity}</span>
+                                        <button onClick={()=>updateQuantity(item.cart_item_id, item.quantity - 1)} className="hover:cursor-pointer"><img src="/icons/dash.svg"></img></button>
+                                    </div>
+                                </div>
                             </div>
                         ))}
+                        </div>
                         </>
                     )}
                 </div>
